9162f40ae007c402993e6550a91ea3fba4dc3e30
eche144 <30297005+eche144@users.noreply.github.com>
2021-10-08 11:30:40 +0200

    Update navigationBar.js

--- components/navigationBar.js
+++ components/navigationBar.js
@@ -3,9 +3,9 @@ import React from 'react'
 const NavigationBar = () => {
   return (
     <nav className="navigation-bar">
-      <a href="#" className="navigation-bar__link">About GCL</a>
+      <a href="#" className="navigation-bar__link">About LINC</a>
       <a href="#" className="navigation-bar__link">Statement of faith</a>
-      <a href="#" className="navigation-bar__link">Calendar of activities</a>
+      <a href="#" className="navigation-bar__link">Project Dates</a>
       <a href="#" className="navigation-bar__link">Our stories</a>
       <a href="#" className="navigation-bar__link">Get in touch</a>
     </nav>
